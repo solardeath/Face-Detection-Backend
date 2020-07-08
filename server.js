f8826f8b73c74bf4246a172e9dd53fae86da91ae
Pronnoy <pronnoy1998@gmail.com>
2020-07-08 13:10:10 +0530

    updating database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-elliptical-80249',
-        user : 'postgres',
-        password : 'test',
-        database : 'face-recognition'
+        host: process.env.DATABASE_URL,
+        ssl:true
     }
 });
 
