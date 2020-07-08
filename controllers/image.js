11bf83715082dbc92ed8bcd044f12ee39f3f2066
Pronnoy <pronnoy1998@gmail.com>
2020-07-08 11:32:27 +0530

    Moved key from frontend to backend and created controllers folders

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,20 @@
+const Clarifai = require('clarifai');
+const { json } = require('body-parser');
+
+const app = new Clarifai.App({
+    apiKey: "9215d097ec2440a0892ed4f270dfd74c",
+});
+
+const handleAPICall = (req,res) =>{
+    app.models
+        .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => res.status(400).json('Unable to work with api'))
+}
+
+
 const handleImage = (req, res,db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
